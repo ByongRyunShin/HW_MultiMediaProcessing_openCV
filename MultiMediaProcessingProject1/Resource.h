39b71b6346ec70919f07b1929acae692c3a97435
ByongRyun <nj298@kumoh.ac.kr>
2017-11-20 19:15:51 +0900

    opencv Environment Setting

--- MultiMediaProcessingProject1/Resource.h
+++ MultiMediaProcessingProject1/Resource.h
@@ -1,21 +1,46 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// MultiMediaProcessingProject1.rc에서 사용됩니다.
+// MultiMediaProcessingProject1.rc에서 사용되고 있습니다.
 //
-#define IDR_MAINFRAME					128
-#define IDM_ABOUTBOX					0x0010
-#define IDD_ABOUTBOX					100
-#define IDS_ABOUTBOX					101
-#define IDD_MULTIMEDIAPROCESSINGPROJECT1_DIALOG				102
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MULTIMEDIAPROCESSINGPROJECT1_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       129
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_PIXEL32780                   32780
+#define ID_PIXEL32781                   32781
+#define ID_PIXEL32782                   32782
+#define ID_PIXEL32783                   32783
+#define ID_NEIGHBORHOOD32784            32784
+#define ID_NEIGHBORHOOD32785            32785
+#define ID_NEIGHBORHOOD32786            32786
+#define ID_NEIGHBORHOOD32787            32787
+#define ID_NEIGHBORHOOD32788            32788
+#define ID_EDGE32789                    32789
+#define ID_EDGE32790                    32790
+#define ID_MORPHOLOGY32791              32791
+#define ID_MORPHOLOGY32792              32792
+#define ID_GEOMETRIC32793               32793
+#define ID_GEOMETRIC32794               32794
+#define ID_GEOMETRIC32795               32795
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32796
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
